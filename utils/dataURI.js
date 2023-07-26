cfc430bdc486769d48bec161209bac02d7505524
SurajRawat12345 <115080037+SurajRawat12345@users.noreply.github.com>
2023-07-26 18:33:07 +0530

    Solving Image Problem

--- utils/dataURI.js
+++ utils/dataURI.js
@@ -1,8 +1,8 @@
-import DataURIParser from 'datauri/parser.js';
+import DataUriParser from 'datauri/parser.js';
 import path from "path";
 
 const getDataUri = (file) => {
-    const parser = new DataURIParser();
+    const parser = new DataUriParser();
     const extName = path.extname(file.originalname).toString();
     return parser.format(extName , file.buffer);
 }
