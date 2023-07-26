7e246f75da2b05465b4e3f40c4fd99a5519a86c0
SurajRawat12345 <115080037+SurajRawat12345@users.noreply.github.com>
2023-07-26 17:12:29 +0530

    solving Image Problem

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 app.use(cookieParser());
 app.use(morgan('dev'));
 
