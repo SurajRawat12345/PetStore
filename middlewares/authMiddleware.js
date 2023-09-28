6685a0e74b7e299f1f61b24dcf6c559a73b159cc
SurajRawat12345 <115080037+SurajRawat12345@users.noreply.github.com>
2023-09-28 12:00:03 +0530

    correcting functions

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -11,8 +11,15 @@ export const requireSignIn = async(req, res , next) => {
                 msg : "please login first",
             })
         }
-        const decode = await JWT.verify(token , process.env.JWT_SECRET);
-        req.user = decode; // Removes undefined_id or decoding
+        const verifyUser = await JWT.verify(token , process.env.JWT_SECRET);
+        const rootUser = await userModel.findOne({_id : verifyUser._id});
+        if(!rootUser){
+            return res.status(401).send({
+                success: false,
+                msg: "User Not Found",
+            })
+        }
+        req.user = verifyUser; // Removes undefined_id or decoding
         next();
     }
     catch(error){
